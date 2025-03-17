e5d70d03a41f969ff3766cdbe0caa2b414a9480d
woomin33 <woomin013@gmail.com>
2025-03-17 17:45:23 +0900

    ✨ feat: coupon component 제작 resolves #28

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,24 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [tailwindcss()],
   build: {
     rollupOptions: {
       input: {
-        index: "index.html", // 기본 index.html
-        common: "src/components/common/common.html",
-        event: "src/components/event/event.html",
-        feed: "src/components/feed/feed.html",
-        login: "src/components/login/login.html",
-        reserve: "src/components/reserve/reserve.html",
-        review: "src/components/review/review.html",
-        visit: "src/components/visit/visit.html",
+        index: 'index.html', // 기본 index.html
+        common: 'src/components/common/common.html',
+        event: 'src/components/event/event.html',
+        feed: 'src/components/feed/feed.html',
+        login: 'src/components/login/login.html',
+        reserve: 'src/components/reserve/reserve.html',
+        review: 'src/components/review/review.html',
+        visit: 'src/components/visit/visit.html',
+        coupon: 'src/components/coupon/coupon.html',
       },
     },
   },
-  appType: "mpa", // fallback 사용안함
+  appType: 'mpa', // fallback 사용안함
   server: {
     // open: 'src/pages/main/index.html', // 서버 시작 시 브라우저에서 지정페이지 자동으로 열기
   },
